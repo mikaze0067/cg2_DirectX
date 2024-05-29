3e3e7c2731998e797df9fbe34a57d24a58fecd95
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-05-29 14:11:43 +0900

    ,をつけた

--- enc_temp_folder/7ca38a6ec69f39a53bc5ccd812e4b48e/main.cpp
+++ enc_temp_folder/7ca38a6ec69f39a53bc5ccd812e4b48e/main.cpp
@@ -88,7 +88,7 @@ IDxcBlob* CompileShader(
 		L"-E",L"main", //エントリーポイントの指定、基本的にmain以外にはしない
 		L"-T",profile, //ShaderProfileの設定
 		L"-Zi",L"-Qembed_debug", //デバッグ用の情報を埋め込む
-		L"-Od",   //最適化を外しておく
+		L"-Od"   //最適化を外しておく
 		L"-Zpr"  //メモリレイアウトは行優先
 	};
 	//実際にShaderをコンパイルする
