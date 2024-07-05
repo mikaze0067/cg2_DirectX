ea3b1335cc9b1f4b594d022c88989cb2921fa2c3
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-07-05 15:57:33 +0900

    途中

--- math/OrthographicMatrix.h
+++ math/OrthographicMatrix.h
@@ -2,19 +2,10 @@
 #include "Matrix4x4.h"
 
 Matrix4x4 MakeOrthograhicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
-	Matrix4x4 ans;
+	Matrix4x4 ans = { 0 };
 	ans.m[0][0] = 2 / (right - left);
-	ans.m[0][1] = 0;
-	ans.m[0][2] = 0;
-	ans.m[0][3] = 0;
-	ans.m[1][0] = 0;
 	ans.m[1][1] = 2 / (top - bottom);
-	ans.m[1][2] = 0;
-	ans.m[1][3] = 0;
-	ans.m[2][0] = 0;
-	ans.m[2][1] = 0;
 	ans.m[2][2] = 1 / (farClip - nearClip);
-	ans.m[2][3] = 0;
 	ans.m[3][0] = (left + right) / (left - right);
 	ans.m[3][1] = (top + bottom) / (bottom - top);
 	ans.m[3][2] = nearClip / (nearClip - farClip);
