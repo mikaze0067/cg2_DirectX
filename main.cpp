9d509dd8c913242f68800780656ec7395f698f1e
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-07-24 14:08:36 +0900

    totyuu

--- main.cpp
+++ main.cpp
@@ -875,6 +875,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//単位行列を書き込んでおく
 	*transformationMatrixDataSprite = MakeIdentity4x4();
 
+	////経度分割１つ分の角度φ（ファイ）
+	//const float kLonEvery = pi * 2.0f / float(kSubdivision);
+	////緯度分割１つ分の角度θ（シータ）
+	//const float kLatEvery = pi / float(kSubdivision);
+	////緯度の方向に分割
+	//for (latIndex = 0; lanIndex < kSubdivision; ++latIndex) {
+	//	float lat = -pi / 2.0f + kLatEvery * latIndex;//θ
+	//}
+
+
 #pragma region ImGuiの初期化
 
 	//ImGuiの初期化
