22d9ecc9d291ad2afb5f0cfd42fec31148f9a205
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-10-21 14:20:02 +0900

    2-1完了

--- Input.h
+++ Input.h
@@ -2,16 +2,12 @@
 #include <windows.h>
 #include <wrl.h>
 
-using namespace Microsoft::WRL;
 
 
 //入力
 class Input {
 public: //メンバ変数
 	
-
-	template<class T> using Comptr = Microsoft::WRL::ComPtr<T>;
-
 	//初期化
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
 	//更新
